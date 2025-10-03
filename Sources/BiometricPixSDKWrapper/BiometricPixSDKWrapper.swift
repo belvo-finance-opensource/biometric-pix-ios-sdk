9ef73448f8a96c0d9413879d46d58d9b4b66d0d3
Romulo Messias <romulo.souza@belvo.com>
2025-10-03 17:00:26 -0300

    feat: update target wrapper

--- Sources/BiometricPixSDKWrapper/BiometricPixSDKWrapper.swift
+++ Sources/BiometricPixSDKWrapper/BiometricPixSDKWrapper.swift
@@ -1,4 +1,4 @@
-// BiometricPixSDK.swift
+// BiometricPixSDKWrapper.swift
 // This wrapper ensures Datadog is available as a transitive dependency
 
 import BiometricPixSDK
@@ -12,3 +12,7 @@ import DatadogCrashReporting
 // 1. Your binary framework is available
 // 2. Datadog is available as a transitive dependency
 // 3. iOS projects get both dependencies automatically
+
+// The actual fix: explicitly expose the class that can be instantiated
+// This resolves the "Cannot call value of non-function type 'module<BiometricPixSDK>'" error
+public typealias BiometricPixSDK = BiometricPixSDK.BiometricPixSDK
