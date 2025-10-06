828a3261c69045241b06670136803da604a7f8d2
Romulo Messias <romulo.souza@belvo.com>
2025-10-06 12:01:02 -0300

    chore: remove unused code

--- Sources/BiometricPixSDKWrapper/BiometricPixSDKWrapper.swift
+++ Sources/BiometricPixSDKWrapper/BiometricPixSDKWrapper.swift
@@ -1,17 +1,5 @@
-// BiometricPixSDKWrapper.swift
-// This wrapper ensures Datadog is available as a transitive dependency
-
 import BiometricPixSDK
 import DatadogObjc
 import DatadogCrashReporting
 
-// Re-export all public APIs from the binary framework
 @_exported import BiometricPixSDK
-
-// This wrapper target ensures that:
-// 1. Your binary framework is available
-// 2. Datadog is available as a transitive dependency
-// 3. iOS projects get both dependencies automatically
-
-// The @_exported import should be sufficient to expose the BiometricPixSDK class
-// If the error persists, the issue might be in how the binary framework is structured
